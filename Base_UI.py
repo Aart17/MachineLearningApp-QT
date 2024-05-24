a9b221ffbe1dc473368a70f472a542fa83b5bddd
Aart17 <artemius2019@mail.ru>
2024-05-24 09:28:19 +0300

    Create Base_UI file

---
+++ Base_UI.py
@@ -0,0 +1,31 @@
+from PyQt5.QtWidgets import *
+
+from modules.FramelessWindow import FramelessWindow
+
+
+class BaseUI(QWidget):
+    def __init__(self, *args, **kwargs):
+        super(BaseUI, self).__init__(*args, **kwargs)
+
+        self.obj = args[0].titleBar
+        self.MainWindow = args[0]
+        self.MainWindow.setWindowTitle("<b>NeuraMade app</b>")
+        self.MainWindow.setGeometry(1, 1, 860, 600)
+
+    def align(self):
+        win = self.MainWindow
+        desk = QApplication.desktop()
+        x = (desk.width() - win.width()) // 2
+        y = (desk.height() - win.height()) // 2
+        self.MainWindow.move((x, y), filter=True)
+
+
+if __name__ == "__main__":
+    from sys import argv, exit
+    from PyQt5.QtWidgets import QApplication, QWidget
+
+    app = QApplication(argv)
+    w = FramelessWindow()  # Модуль для создания окон без рамок
+    w.setWidget(BaseUI(w))  # Добавить свое окно
+    w.show()
+    exit(app.exec_())
